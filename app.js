8688eb07abf4ea694db149252bb2b67297609f36
Krzysztof Chybicki <krysiek97@interia.eu>
2020-04-26 23:54:45 +0200

    test

--- app.js
+++ app.js
@@ -12,32 +12,33 @@ const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_U
 var authed = false;
 
 app.get('/', (req, res) => {
-    if (!authed) {
-        // Generate an OAuth URL and redirect there
-        const url = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: 'https://www.googleapis.com/auth/gmail.readonly'
-        });
-        console.log(url)
-        res.redirect(url);
-    } else {
-        const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
-        gmail.users.labels.list({
-            userId: 'me',
-        }, (err, res) => {
-            if (err) return console.log('The API returned an error: ' + err);
-            const labels = res.data.labels;
-            if (labels.length) {
-                console.log('Labels:');
-                labels.forEach((label) => {
-                    console.log(`- ${label.name}`);
-                });
-            } else {
-                console.log('No labels found.');
-            }
-        });
-        res.send('Logged in')
-    }
+    // if (!authed) {
+    //     // Generate an OAuth URL and redirect there
+    //     const url = oAuth2Client.generateAuthUrl({
+    //         access_type: 'offline',
+    //         scope: 'https://www.googleapis.com/auth/gmail.readonly'
+    //     });
+    //     console.log(url)
+    //     res.redirect(url);
+    // } else {
+    //     const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+    //     gmail.users.labels.list({
+    //         userId: 'me',
+    //     }, (err, res) => {
+    //         if (err) return console.log('The API returned an error: ' + err);
+    //         const labels = res.data.labels;
+    //         if (labels.length) {
+    //             console.log('Labels:');
+    //             labels.forEach((label) => {
+    //                 console.log(`- ${label.name}`);
+    //             });
+    //         } else {
+    //             console.log('No labels found.');
+    //         }
+    //     });
+    //     res.send('Logged in')
+    // }
+    res.send("adsadsa")
 })
 
 app.get('/auth/google/callback', function (req, res) {
