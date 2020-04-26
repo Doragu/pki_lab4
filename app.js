44a6ee7d604960b9c5d0ba4cdce70e3ccc27b089
Krzysztof Chybicki <krysiek97@interia.eu>
2020-04-26 23:59:32 +0200

    status

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const { google } = require('googleapis');
+// const { google } = require('googleapis');
 const express = require('express')
-const OAuth2Data = require('./google_key.json')
+// const OAuth2Data = require('./google_key.json')
 
 const app = express()
 
-const CLIENT_ID = OAuth2Data.web.client_id;
-const CLIENT_SECRET = OAuth2Data.web.client_secret;
-const REDIRECT_URL = OAuth2Data.web.redirect_uris[1]
+// const CLIENT_ID = OAuth2Data.web.client_id;
+// const CLIENT_SECRET = OAuth2Data.web.client_secret;
+// const REDIRECT_URL = OAuth2Data.web.redirect_uris[1]
 
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
-var authed = false;
+// const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+// var authed = false;
 
 app.get('/', (req, res) => {
     // if (!authed) {
@@ -41,23 +41,23 @@ app.get('/', (req, res) => {
     res.send("adsadsa")
 })
 
-app.get('/auth/google/callback', function (req, res) {
-    const code = req.query.code
-    if (code) {
-        // Get an access token based on our OAuth code
-        oAuth2Client.getToken(code, function (err, tokens) {
-            if (err) {
-                console.log('Error authenticating')
-                console.log(err);
-            } else {
-                console.log('Successfully authenticated');
-                oAuth2Client.setCredentials(tokens);
-                authed = true;
-                res.redirect('/')
-            }
-        });
-    }
-});
+// app.get('/auth/google/callback', function (req, res) {
+//     const code = req.query.code
+//     if (code) {
+//         // Get an access token based on our OAuth code
+//         oAuth2Client.getToken(code, function (err, tokens) {
+//             if (err) {
+//                 console.log('Error authenticating')
+//                 console.log(err);
+//             } else {
+//                 console.log('Successfully authenticated');
+//                 oAuth2Client.setCredentials(tokens);
+//                 authed = true;
+//                 res.redirect('/')
+//             }
+//         });
+//     }
+// });
 
-const port = process.env.port || 5000
+const port = process.env.port
 app.listen(port, () => console.log(`Server running at ${port}`));
